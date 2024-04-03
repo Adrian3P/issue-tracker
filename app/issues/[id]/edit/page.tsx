79f8eb69db2c1f48f4f70026efb62d0ff14202f2
Adrian Ionut Patrascu <ap@greenbyte.dk>
2024-04-03 19:25:55 +0200

    Improving the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import IssueForm from "../../_components/IssueForm";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
   params: { id: string };
@@ -16,7 +22,7 @@ const EditIssuePage = async ({ params }: Props) => {
 
   return (
     <div>
-          <IssueForm issue={issue} />
+      <IssueForm issue={issue} />
     </div>
   );
 };
