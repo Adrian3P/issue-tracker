94d0ec13a33a3c475139498eeb306a02e2a46db9
Adrian Ionut Patrascu <ap@greenbyte.dk>
2024-04-11 18:15:26 +0200

    Removed duplicate skeletons

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -33,7 +33,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       if (issue) await axios.patch("/api/issue" + issue.id, data);
       else await axios.post("/api/issues", data);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setSubmitting(false);
